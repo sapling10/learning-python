6f5d9effa4340d4c28eb7b61d6a330878f95fdde
Connor Cotturone <ccotturo@vols.utk.edu>
2023-04-01 16:37:05 -0400

    added documentation to 1-temp-converter

--- projects/1-temp-converter.py
+++ projects/1-temp-converter.py
@@ -1,14 +1,16 @@
+# basic converter between F to C or F to C
+# takes in user input for conversion type,
+# than the current temperature
 def main():
     # get what conversion they want to make
     type = input("Convert to F or C?: ")
     while ((type != "C") and (type != "c") and (type != "F") and (type != "f")):
         print(f"{type} is not a valid input, use F or C...")
-        type = input()
+        type = input("Convert to F or C?: ")
     # we have a correct input, get value
-    print("What is your current temperature?: ")
     while True:
         try:
-            value = float(input())
+            value = float(input("What is your current temperature?: "))
             break
         except:
             print("not a valid type, try again...")
@@ -17,7 +19,7 @@ def main():
         print(f"converting F -> C...")
         conversion = (value - 32) * 5/9
         print(f"{value}F -> {round(conversion, 4)}C")
-    elif ((type == "F") or (type == "f")):
+    else:
         print(f"converting C -> F...")
         conversion = (value * 9/5) + 32
         print(f"{value}C -> {round(conversion, 4)}F")
